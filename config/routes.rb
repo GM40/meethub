fd9ac46521d4ad0aa1f068f1a4a44b2b66217ffe
ghulam mohi u din <ghulam.mohiudin@xprolabs.com>
2022-06-08 14:59:58 +0500

    name attribute to user added , added name attribute to signup_params and update_params,

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
