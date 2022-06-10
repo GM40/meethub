5f391edc2252d1f108d3fffdfe12f5e4d286fa18
ghulam mohi u din <ghulam.mohiudin@xprolabs.com>
2022-06-10 16:51:46 +0500

    bootstrap not working issue resolved

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '>= 6'
-gem 'jquery-rails'
+gem 'bootstrap'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,20 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("@popperjs/core")
-import "bootstrap"
+
 // Import the specific modules you may need (Modal, Alert, etc)
 import { Tooltip, Popover } from "bootstrap"
 // The stylesheet location we created earlier
-require("../stylesheets/application.scss")
 //= require jquery
 //= require jquery-ui
-//= require bootstrap
+//= require jquery_ujs
 //= require bootstrap-sprockets
 
 document.addEventListener("turbolinks:load", () => {
